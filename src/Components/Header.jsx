5a9fe257be470de8b0eba2e820358b0b74a2593a
Balavignesh-J <balav3230@gmail.com>
2025-02-17 16:06:34 +0530

    Completed Header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import Logo from "../Img/Logo.png";
+import Profile from "../Img/Profile.png";
+import Search from "../Img/Search.png";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className="flex flex-row justify-between w-full items-center bg-blue-400 h-15">
+      <img
+        src={Logo}
+        alt="Logo"
+        className="w-20 h-20 ml-2"
+      />
 
-export default Header
+      <div className="flex justify-center m-3">
+        <input
+          type="text"
+          placeholder="Search for products"
+          className=" border border-black p-2 bg-stone-100 rounded-l-lg w-150"
+        />
+
+        <button className=" bg-blue-600 rounded-r-lg w-10 flex justify-center items-center cursor-pointer">
+        <img src={Search} alt="Search" className="w-5 h-5" />
+        </button>
+      </div>
+
+      <img src={Profile} title="Profile" alt="profile" className="w-10 h-10 border border-blue-50 mr-2" />
+    </div>
+  );
+};
+
+export default Header;
